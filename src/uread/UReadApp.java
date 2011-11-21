37e0032a8d9273440c787da86f2c22e4c181f804
Tina Gospodinova <tina@zeevex.com>
2011-11-21 13:05:19 -0500

    First import

---
+++ src/uread/UReadApp.java
@@ -0,0 +1,44 @@
+/*
+ * UReadApp.java
+ */
+
+package uread;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ */
+public class UReadApp extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new UReadView(this));
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of UReadApp
+     */
+    public static UReadApp getApplication() {
+        return Application.getInstance(UReadApp.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        launch(UReadApp.class, args);
+    }
+}
